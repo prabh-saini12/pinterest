968bac42a1ddb207a66cb61e4940fdc3c7a8babb
khushiram saini <prabhakarsaini800@gmail.com>
2024-09-23 21:19:04 +0530

    / route added

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ dotenv.config({});
 import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
 import userRoutes from "./routes/user.route.js";
-import pinRoutes from './routes/pin.route.js'
+import pinRoutes from "./routes/pin.route.js";
 import cloudinary from "cloudinary";
-import cors from 'cors'
+import cors from "cors";
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -14,27 +14,28 @@ cloudinary.v2.config({
   api_secret: process.env.API_SECRET,
 });
 
-
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (_, res) => {
+  res.send("Deployment working");
+});
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // routes
 app.use("/api/user", userRoutes);
-app.use("/api/pin",pinRoutes)
+app.use("/api/pin", pinRoutes);
 
 app.listen(PORT, () => {
   connectDB();
