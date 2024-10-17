005e0c7bbdcf3b9a0bc1e40583ffd07599e9c500
khushiram saini <prabhakarsaini800@gmail.com>
2024-10-17 21:07:15 +0530

    Refactor generateToken.js to include secure flag in cookie options

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,7 @@ const generateToken = (id, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
   });
 };
-
-
-export default generateToken
+export default generateToken;
